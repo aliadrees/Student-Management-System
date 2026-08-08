6b459fa954a5307bc3f532c6c6c4f0a70c5b68e1
Ali Adrees <aliadrees4560@gmail.com>
2026-08-08 14:12:06 +0500

    Update file path for data.json in forgot.py

--- forgot.py
+++ forgot.py
@@ -3,7 +3,7 @@ def forgot():
     a=input("\nEnter Your Uername")
 
     try:
-     with open('data.json','r') as f:
+     with open('data/data.json','r') as f:
         users=json.load(f)
      for user in users:
        if user["username"]==a:
@@ -12,7 +12,7 @@ def forgot():
           break
      else:
          print("user Not found")
-     with open('data.json','w') as f:
+     with open('data/data.json','w') as f:
              json.dump(users,f,indent=4)
     except FileNotFoundError:
        print('File Not found')
